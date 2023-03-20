6c1ed3c3cc84dec5ec70e0785201d753d3d10e29
adumbidiot <nathaniel.daniel23@outlook.com>
2023-03-20 00:50:08 -0700

    Add get_volume_information callback wrapper

--- dokany/src/lib.rs
+++ dokany/src/lib.rs
@@ -10,6 +10,7 @@ pub(crate) use self::operations::OPERATIONS;
 pub use self::option_flags::OptionFlags;
 pub use self::options::Options;
 pub use dokany_sys as sys;
+use std::mem::MaybeUninit;
 use std::sync::Once;
 
 /// Implemented for types that can be converted into wide char arrays.
@@ -55,8 +56,126 @@ impl<'a> AsWide for &'a Vec<u16> {
     }
 }
 
+/// A cell that wraps an unitialzed wide c string buffer, tracking its initialization.
+pub struct WriteWideCStringCell<'a> {
+    slice: &'a mut [MaybeUninit<u16>],
+    initialized: bool,
+}
+
+impl<'a> WriteWideCStringCell<'a> {
+    fn new(slice: &'a mut [MaybeUninit<u16>]) -> Self {
+        Self {
+            slice,
+            initialized: false,
+        }
+    }
+
+    /// Write the contents of an AsWide into this buffer, marking it as initialized.
+    ///
+    /// If the AsWide is too long, it is truncated.
+    pub fn write(&mut self, value: impl AsWide) {
+        // Reserve NUL
+        let max_len = self.slice.len() - 1;
+        for (buffer, value) in self
+            .slice
+            .iter_mut()
+            .zip(value.as_wide().take(max_len).chain(std::iter::once(0)))
+        {
+            buffer.write(value);
+        }
+        self.initialized = true;
+    }
+
+    /// Get the total length of the buffer, in wide chars
+    pub fn total_len(&self) -> usize {
+        self.slice.len()
+    }
+}
+
+bitflags::bitflags! {
+    /// Capability flags for a filesystem.
+    ///
+    /// FILE_FILE_COMPRESSION and FILE_VOL_IS_COMPRESSED are mutually exclusive
+    #[repr(transparent)]
+    pub struct FileSystemFlags: sys::DWORD {
+        /// The specified volume supports case-sensitive file names.
+        const CASE_SENSITIVE_SEARCH = sys::FILE_CASE_SENSITIVE_SEARCH;
+        /// The specified volume supports preserved case of file names when it places a name on disk.
+        const CASE_PRESERVED_NAMES = sys::FILE_CASE_PRESERVED_NAMES;
+        /// The specified volume supports Unicode in file names as they appear on disk.
+        const UNICODE_ON_DISK = sys::FILE_UNICODE_ON_DISK;
+        /// The specified volume preserves and enforces access control lists (ACL).
+        ///
+        /// For example, the NTFS file system preserves and enforces ACLs, and the FAT file system does not.
+        const PERSISTENT_ACLS = sys::FILE_PERSISTENT_ACLS;
+        /// The specified volume supports file-based compression.
+        const FILE_COMPRESSION = sys::FILE_FILE_COMPRESSION;
+        /// The specified volume supports disk quotas.
+        const VOLUME_QUOTAS = sys::FILE_VOLUME_QUOTAS;
+        /// The specified volume supports sparse files.
+        const SUPPORTS_SPARSE_FILES = sys::FILE_SUPPORTS_SPARSE_FILES;
+        /// The specified volume supports reparse points.
+        const SUPPORTS_REPARSE_POINTS = sys::FILE_SUPPORTS_REPARSE_POINTS;
+        /// ?
+        const SUPPORTS_REMOTE_STORAGE = sys::FILE_SUPPORTS_REMOTE_STORAGE;
+        /// ?
+        const RETURNS_CLEANUP_RESULT_INFO = sys::FILE_RETURNS_CLEANUP_RESULT_INFO;
+        /// ?
+        const SUPPORTS_POSIX_UNLINK_RENAME = sys::FILE_SUPPORTS_POSIX_UNLINK_RENAME;
+        /// The specified volume is a compressed volume, for example, a DoubleSpace volume.
+        const VOLUME_IS_COMPRESSED = sys::FILE_VOLUME_IS_COMPRESSED;
+        /// The specified volume supports object identifiers.
+        const SUPPORTS_OBJECT_IDS = sys::FILE_SUPPORTS_OBJECT_IDS;
+        /// The specified volume supports the Encrypted File System (EFS). For more information, see File Encryption.
+        const SUPPORTS_ENCRYPTION = sys::FILE_SUPPORTS_ENCRYPTION;
+        /// The specified volume supports named streams.
+        const FILE_NAMED_STREAMS = sys::FILE_NAMED_STREAMS;
+        /// The specified volume is read-only.
+        const FILE_READ_ONLY_VOLUME = sys::FILE_READ_ONLY_VOLUME;
+        /// The specified volume supports a single sequential write.
+        const SEQUENTIAL_WRITE_ONCE = sys::FILE_SEQUENTIAL_WRITE_ONCE;
+        /// The specified volume supports transactions. For more information, see About KTM.
+        const SUPPORTS_TRANSACTIONS = sys::FILE_SUPPORTS_TRANSACTIONS;
+        /// The specified volume supports hard links. For more information, see Hard Links and Junctions.
+        const FILE_SUPPORTS_HARD_LINKS = sys::FILE_SUPPORTS_HARD_LINKS;
+        /// The specified volume supports extended attributes.
+        ///
+        /// An extended attribute is a piece of application-specific metadata that an application can associate with a file and is not part of the file's data.
+        const SUPPORTS_EXTENDED_ATTRIBUTES = sys::FILE_SUPPORTS_EXTENDED_ATTRIBUTES;
+        /// The file system supports open by FileID. For more information, see FILE_ID_BOTH_DIR_INFO.
+        const SUPPORTS_OPEN_BY_FILE_ID = sys::FILE_SUPPORTS_OPEN_BY_FILE_ID;
+        /// The specified volume supports update sequence number (USN) journals. For more information, see Change Journal Records.
+        const SUPPORTS_USN_JOURNAL = sys::FILE_SUPPORTS_USN_JOURNAL;
+        /// ?
+        const SUPPORTS_INTEGRITY_STREAMS = sys::FILE_SUPPORTS_INTEGRITY_STREAMS;
+        /// The specified volume supports sharing logical clusters between files on the same volume.
+        ///
+        /// The file system reallocates on writes to shared clusters.
+        /// Indicates that FSCTL_DUPLICATE_EXTENTS_TO_FILE is a supported operation.
+        const SUPPORTS_BLOCK_REFCOUNTING = sys::FILE_SUPPORTS_BLOCK_REFCOUNTING;
+        /// ?
+        const SUPPORTS_SPARSE_VDL = sys::FILE_SUPPORTS_SPARSE_VDL;
+        /// The specified volume is a direct access (DAX) volume.
+        const DAX_VOLUME = sys::FILE_DAX_VOLUME;
+        /// ?
+        const SUPPORTS_GHOSTING = sys::FILE_SUPPORTS_GHOSTING;
+    }
+}
+
 /// The trait a type must implement to serve as a filesystem
 pub trait Filesystem: Send + Sync + 'static {
+    /// Called for calls to GetVolumeInformation
+    fn get_volume_information(
+        &self,
+        _volume_name: WriteWideCStringCell<'_>,
+        _volume_serial_number: &mut u32,
+        _maximum_component_length: &mut u32,
+        _file_system_flags: &mut FileSystemFlags,
+        _file_system_name: WriteWideCStringCell<'_>,
+    ) -> sys::NTSTATUS {
+        sys::STATUS_NOT_IMPLEMENTED
+    }
+
     /// Called when the filesystem is mounted
     fn mounted(&self, _mount_point: &[u16]) -> sys::NTSTATUS {
         sys::STATUS_SUCCESS
@@ -153,6 +272,28 @@ mod test {
     struct SimpleFilesystem;
 
     impl Filesystem for SimpleFilesystem {
+        fn get_volume_information(
+            &self,
+            mut volume_name: WriteWideCStringCell<'_>,
+            volume_serial_number: &mut u32,
+            maximum_component_length: &mut u32,
+            file_system_flags: &mut FileSystemFlags,
+            mut file_system_name: WriteWideCStringCell<'_>,
+        ) -> sys::NTSTATUS {
+            volume_name.write("Simple Filesystem");
+
+            *volume_serial_number = 1337;
+            *maximum_component_length = 255;
+
+            *file_system_flags |= FileSystemFlags::UNICODE_ON_DISK;
+
+            file_system_name.write("NTFS");
+
+            println!("GetVolumeInformation");
+
+            sys::STATUS_SUCCESS
+        }
+
         fn mounted(&self, mounted: &[u16]) -> sys::NTSTATUS {
             println!("Mounted at {:?}", OsString::from_wide(mounted));
             sys::STATUS_SUCCESS
--- dokany/src/operations.rs
+++ dokany/src/operations.rs
@@ -1,8 +1,11 @@
 use crate::sys;
 use crate::GlobalContext;
+use crate::WriteWideCStringCell;
+use std::mem::MaybeUninit;
 
 /// Function trampolines
 pub(crate) static OPERATIONS: sys::DOKAN_OPERATIONS = sys::DOKAN_OPERATIONS {
+    GetVolumeInformation: Some(get_volume_information_callback),
     Mounted: Some(mounted_callback),
     Unmounted: Some(unmounted_callback),
     ..sys::DOKAN_OPERATIONS::new()
@@ -22,6 +25,54 @@ unsafe fn extract_global_context<'a>(
     let options = &*options;
     debug_assert!(options.GlobalContext != 0);
     &mut *(options.GlobalContext as *mut GlobalContext)
+}
+
+unsafe extern "stdcall" fn get_volume_information_callback(
+    volume_name_buffer: sys::LPWSTR,
+    volume_name_size: sys::DWORD,
+    volume_serial_number: sys::LPDWORD,
+    maximum_component_length: sys::LPDWORD,
+    file_system_flags: sys::LPDWORD,
+    file_system_name_buffer: sys::LPWSTR,
+    file_system_name_size: sys::DWORD,
+    dokan_file_info: sys::PDOKAN_FILE_INFO,
+) -> sys::NTSTATUS {
+    let result = std::panic::catch_unwind(|| {
+        let global_context = extract_global_context(dokan_file_info);
+
+        let volume_name = std::slice::from_raw_parts_mut(
+            volume_name_buffer.cast::<MaybeUninit<u16>>(),
+            volume_name_size.try_into().unwrap(),
+        );
+        volume_name[0].write(0);
+        let volume_name = WriteWideCStringCell::new(volume_name);
+
+        // Defaults chosen from memfs example
+        volume_serial_number.write(0x19831116);
+        maximum_component_length.write(255);
+
+        file_system_flags.write(0);
+
+        let file_system_name = std::slice::from_raw_parts_mut(
+            file_system_name_buffer.cast::<MaybeUninit<u16>>(),
+            file_system_name_size.try_into().unwrap(),
+        );
+        file_system_name[0].write(0);
+        let file_system_name = WriteWideCStringCell::new(file_system_name);
+
+        global_context.filesystem.get_volume_information(
+            volume_name,
+            &mut *volume_serial_number,
+            &mut *maximum_component_length,
+            &mut *file_system_flags.cast(),
+            file_system_name,
+        )
+    });
+
+    match result {
+        Ok(code) => code,
+        Err(_e) => sys::STATUS_INTERNAL_ERROR,
+    }
 }
 
 unsafe extern "stdcall" fn mounted_callback(
