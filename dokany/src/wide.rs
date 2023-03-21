9db2e089666da10a84c4d1250a7931119b39535a
adumbidiot <nathaniel.daniel23@outlook.com>
2023-03-20 20:25:48 -0700

    Impl AsWide for Path and PathBuf

--- dokany/src/wide.rs
+++ dokany/src/wide.rs
@@ -1,7 +1,8 @@
-
 use std::ffi::OsStr;
 use std::ffi::OsString;
 use std::os::windows::ffi::OsStrExt;
+use std::path::Path;
+use std::path::PathBuf;
 
 /// Implemented for types that can be converted into wide char arrays.
 pub trait AsWide {
@@ -59,5 +60,21 @@ impl<'a> AsWide for &'a OsString {
 
     fn as_wide(&self) -> Self::Iter {
         self.encode_wide()
+    }
+}
+
+impl<'a> AsWide for &'a Path {
+    type Iter = std::os::windows::ffi::EncodeWide<'a>;
+
+    fn as_wide(&self) -> Self::Iter {
+        self.as_os_str().encode_wide()
+    }
+}
+
+impl<'a> AsWide for &'a PathBuf {
+    type Iter = std::os::windows::ffi::EncodeWide<'a>;
+
+    fn as_wide(&self) -> Self::Iter {
+        self.as_os_str().encode_wide()
     }
 }
